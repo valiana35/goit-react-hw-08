7e98ec032c9b15be532cb01613351ebf885b9a45
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-09-12 14:53:49 +0300

    link

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://connections-api.goit.global";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com/";
+
+// "https://connections-api.herokuapp.com/"
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
